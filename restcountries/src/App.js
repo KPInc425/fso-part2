f2057861bef1ff779fd77be33c57334f16cb25ce
KPInc425 <KPInc425@gmail.com>
2022-10-31 02:54:16 -0700

    Implement exercise 2.12 solution: Data for countries RESTAPI

---
+++ restcountries/src/App.js
@@ -0,0 +1,116 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [newSearch, setNewSearch] = useState('');
+  const [showFiltered, setShowFiltered]  = useState(false);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
+
+  useEffect(() => {
+    axios
+    .get("https://restcountries.com/v3.1/all")
+    .then(response => {
+      // console.log(typeof JSON.parse(response.data));
+      setCountries(response.data);
+    })
+  }, []);
+
+  const handleSearchChange = (e) => {
+    console.log(e.target.value);
+    setNewSearch(e.target.value);
+    if (e.target.value.length > 0) {
+      setShowFiltered(true);
+      console.log(countries.filter((country) => country.name.common.slice(0, newSearch.length).toLowerCase() === newSearch));
+      setFilteredCountries(countries.filter((country) => country.name.common.slice(0, newSearch.length).toLowerCase() === newSearch));
+    } else {
+      setShowFiltered(false);
+      setNewSearch('');
+      setFilteredCountries([]);
+    }
+
+  }
+
+  console.log(filteredCountries);
+
+  return (
+    <div className="App">
+      <form>
+        Find Countries: <input placeholder="Input country name..." onChange={ handleSearchChange }></input>
+      </form>
+      <Entries filteredCountries={ filteredCountries } /> 
+    </div>
+  );
+}
+
+const Entries = ({ filteredCountries }) => {
+  console.log(filteredCountries);
+  if (filteredCountries.length <= 0) {
+    return (
+      <div><p>You need to enter a country name</p></div>
+    )
+  } else if (filteredCountries.length > 10) {
+    return (
+      <div><p>Too many matches, specify country.</p></div>
+    )
+  } else if (filteredCountries.length === 1) {
+    let country = filteredCountries[0];
+    return (
+      <ChosenEntry name={ country.name.common } capital={ country.capital[0] } area={ country.area } languages={ country.languages} flag={country.flags.png} />
+    )
+  } else {
+    return (
+      <div>
+        { filteredCountries.map((country) => <Entry key={country.cca2} name={country.name.common} />)}
+      </div>
+    )
+  } 
+}
+
+const Entry = ({ name }) => {
+  return (
+    <div>
+      <p> { name }</p>
+    </div>
+  )
+}
+
+const ChosenEntry = ({name, capital, area, languages, flag}) => {
+  console.log(languages);
+  console.log(flag);
+  return (
+    <div>
+      <h2> {name} </h2>
+      <p>Capital: {capital}</p>
+      <p>Area: {area}</p>
+
+      <h3>Languages:</h3>
+      <ul>
+        <Languages languages={ languages } />
+      </ul>
+      <img src={ flag } alt={ 'flag' } />
+    </div>
+  )
+}
+
+const Languages = ({ languages }) => {
+  console.log(languages);
+  const languagesArray = [];
+  for (let item in languages) {
+    languagesArray.push([item, languages[item]]);
+  }
+  console.log(languagesArray);
+
+  return (
+    <div>
+      {languagesArray.map((language) => <li key={language[0]}> { language[1] } </li>)}
+    </div>
+  )
+}
+
+// const language
+
+
+export default App;
